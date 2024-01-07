1b95c36c7d7e6538237633915929ba8c4d34c6d0
lesliemw <l.marie1598@gmail.com>
2024-01-07 22:22:48 +0000

    update files

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,16 +1,25 @@
 const Item = require("../models/ItemModel");
+const axios = require("axios");
 
 //@desc   get item data
 //@route  GET /api/cart/cart
 async function getItem(req, res) {
-  const { product, size, quantity, price } = req.body;
-  const item = await Item.find({ user: req.user.id });
-  res.status(200).json(item);
+  try {
+    const item = await axios.get("https://fakestoreapi.com/products");
+    const items = await Item.insertMany(item.data);
+    res.status(200).json(items);
+  } catch (e) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Error" });
+  }
 }
 
 //@desc     add item data
 //@route    POST /api/items/item/:id
-async function addItem(req, res) {}
+async function addItem(req, res) {
+  const item = axios.post("https://fakestoreapi.com/products");
+  res.status(200).json(item);
+}
 
 //@desc     delete item data
 //@route    DELETE /api/items/removeItem
